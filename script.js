19f6ba719a35378be41c80b7751cef55922b67df
Melese Berehannu <melemeko12@gmail.com>
2023-05-22 19:25:24 +0100

    TV Show

--- script.js
+++ script.js
@@ -6,23 +6,31 @@ const showContainer = document.querySelector("[data-show-container]");
 const searchInput = document.querySelector("[data-search]");
 const selectepisodes = document.querySelector("[selectEpisode]");
 const selectshows = document.querySelector("[selectShow]");
-const allShows = getAllShows();
 
-allShows.sort(function (a, b) {
-  var alphabeticSort = a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+// Fetching allShows
 
-  return alphabeticSort;
-});
+fetch(`http://api.tvmaze.com/shows`)
+  .then((res) => res.json())
+  .then((allShows) => {
+    allShows
+      .sort(function (a, b) {
+        var alphabeticSort = a.name
+          .toLowerCase()
+          .localeCompare(b.name.toLowerCase());
 
-allShows.forEach((show) => {
-  const selectshows = document.getElementById("mySelectShow");
-  const selectOption2 = document.createElement("option");
+        return alphabeticSort;
+      })
+      .forEach((show) => {
+        const selectshows = document.getElementById("mySelectShow");
+        const selectOption2 = document.createElement("option");
 
-  selectshows.appendChild(selectOption2);
+        selectshows.appendChild(selectOption2);
 
-  selectOption2.setAttribute("value", `${show.id}`);
-  selectOption2.innerHTML = `${show.name}`;
-});
+        selectOption2.setAttribute("value", `${show.id}`);
+        selectOption2.innerHTML = `${show.name}`;
+      });
+  });
+
 function showFunction() {
   let showId = document.getElementById("mySelectShow").value;
   if (showId === "blanckOption-0-0") {
@@ -68,6 +76,7 @@ function showFunction() {
     fetchEpisodes(showId);
   }
 }
+
 function myFunction() {
   let x = document.getElementById("mySelect").value;
   const xvalue = x.split("-")[1];
@@ -102,6 +111,7 @@ searchInput.addEventListener("input", (e) => {
     episode.element.classList.toggle("hide", !isTrue);
   });
 });
+
 function fetchEpisodes(showId) {
   fetch(`http://api.tvmaze.com/shows/${showId}/episodes`)
     .then((res) => res.json())
@@ -147,13 +157,82 @@ function fetchEpisodes(showId) {
         };
       });
     });
-  //window.onload = setup;
 }
+
 function fetchShows() {
   fetch(`http://api.tvmaze.com/shows`)
     .then((res) => res.json())
     .then((data) => {
-      episodes = data.map((episode) => {
+      episodes = data
+        .sort(function (a, b) {
+          var alphabeticSort = a.name
+            .toLowerCase()
+            .localeCompare(b.name.toLowerCase());
+
+          return alphabeticSort;
+        })
+        .map((episode) => {
+          const container = showTemplate.content.cloneNode(true).children[0];
+          const header = container.querySelector("[data-header]");
+          const image = container.querySelector("[data-image]");
+          const para = container.querySelector("[data-para]");
+          const link = container.querySelector("[data-link]");
+          const rated = container.querySelector("[data-rated]");
+          const gener = container.querySelector("[data-gener]");
+          const status = container.querySelector("[data-status]");
+          const runtime = container.querySelector("[data-runtime]");
+
+          header.innerHTML = `${episode.name}`;
+          rated.innerHTML = `Rated: ${episode.rating.average}`;
+          gener.innerHTML = `Gener: ${episode.geners}`;
+          status.innerHTML = `Status: ${episode.status}`;
+          runtime.innerHTML = `Runtime: ${episode.runtime}`;
+
+          const selectEpisode = document.getElementById("mySelect");
+          const selectOption = document.createElement("option");
+          selectEpisode.appendChild(selectOption);
+          // header.setAttribute("onclick", "showFunction()");
+          selectOption.setAttribute(
+            "value",
+            `S0${episode.season}E0${episode.number}-${episode.name}`
+          );
+          selectOption.innerHTML = `S0${episode.season}E0${episode.number} - ${episode.name}`;
+          selectOption.addEventListener("click", (e) => {
+            const x = e.target.value;
+            const isFalse =
+              `S0${episode.season}E0${episode.number} - ${episode.name}` === x;
+
+            episode.element.classList.toggle("hide", !isFalse);
+          });
+          image.src = episode.image.medium;
+          para.innerHTML = episode.summary;
+
+          link.setAttribute("href", episode._links.self.href);
+          showContainer.append(container);
+
+          return {
+            name: episode.name,
+            image: episode.image.medium,
+            summary: episode.summary,
+            _links: episode._links.self.href,
+            element: container,
+          };
+        });
+    });
+}
+
+fetch(`http://api.tvmaze.com/shows`)
+  .then((res) => res.json())
+  .then((data) => {
+    episodes = data
+      .sort(function (a, b) {
+        var alphabeticSort = a.name
+          .toLowerCase()
+          .localeCompare(b.name.toLowerCase());
+
+        return alphabeticSort;
+      })
+      .map((episode) => {
         const container = showTemplate.content.cloneNode(true).children[0];
         const header = container.querySelector("[data-header]");
         const image = container.querySelector("[data-image]");
@@ -200,61 +279,6 @@ function fetchShows() {
           element: container,
         };
       });
-    });
-
- window.onload = setup;
-}
-fetch(`http://api.tvmaze.com/shows`)
-.then((res) => res.json())
-.then((data) => {
-  episodes = data.map((episode) => {
-    const container = showTemplate.content.cloneNode(true).children[0];
-    const header = container.querySelector("[data-header]");
-    const image = container.querySelector("[data-image]");
-    const para = container.querySelector("[data-para]");
-    const link = container.querySelector("[data-link]");
-    const rated = container.querySelector("[data-rated]");
-    const gener = container.querySelector("[data-gener]");
-    const status = container.querySelector("[data-status]");
-    const runtime = container.querySelector("[data-runtime]");
-
-    header.innerHTML = `${episode.name}`;
-    rated.innerHTML = `Rated: ${episode.rating.average}`;
-    gener.innerHTML = `Gener: ${episode.geners}`;
-    status.innerHTML = `Status: ${episode.status}`;
-    runtime.innerHTML = `Runtime: ${episode.runtime}`;
-
-    const selectEpisode = document.getElementById("mySelect");
-    const selectOption = document.createElement("option");
-    selectEpisode.appendChild(selectOption);
-
-    selectOption.setAttribute(
-      "value",
-      `S0${episode.season}E0${episode.number}-${episode.name}`
-    );
-    selectOption.innerHTML = `S0${episode.season}E0${episode.number} - ${episode.name}`;
-    selectOption.addEventListener("click", (e) => {
-      const x = e.target.value;
-      const isFalse =
-        `S0${episode.season}E0${episode.number} - ${episode.name}` === x;
-
-      episode.element.classList.toggle("hide", !isFalse);
-    });
-    image.src = episode.image.medium;
-    para.innerHTML = episode.summary;
-
-    link.setAttribute("href", episode._links.self.href);
-    showContainer.append(container);
-
-    return {
-      name: episode.name,
-      image: episode.image.medium,
-      summary: episode.summary,
-      _links: episode._links.self.href,
-      element: container,
-    };
   });
-});
 
 window.onload = setup;
-
